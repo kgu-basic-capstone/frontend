60348d2e6eadc09afebba111d845a0943754f412
cho4u4o <cho4u4o@gmail.com>
2025-04-13 20:39:37 +0900

    feat : initial setting

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import "./font.css";
+
+export const metadata: Metadata = {
+  title: "MediPet",
+  description: "반려동물 건강 관리를 간편하게!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="flex min-h-screen flex-col bg-gray-50">{children}</body>
+    </html>
+  );
+}
