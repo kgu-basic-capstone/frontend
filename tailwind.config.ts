a053b3bd46c434a6c058fc468ff5bff2f93c4c9d
cho4u4o <cho4u4o@gmail.com>
2025-05-30 15:38:27 +0900

    design : add point color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        point: "var(--point)",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         card: {
